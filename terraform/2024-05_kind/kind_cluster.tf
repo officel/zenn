cc5102491c79cc915882b1601743c1bc6803dd5e
NISHIMURA Yoshitaka <officel@users.noreply.github.com>
2024-05-20 00:28:10 +0900

    blog(terraform_jp): manage kind with terraform
    fix: #21
    
    今週分の terraform-jp の記事
    
    kind をコマンドで扱う際に yaml を更新して管理を忘れたり、差分を管理するのにコメントを入れそこねたりするので
    慣れてる terraform で管理する試み。

---
+++ terraform/2024-05_kind/kind_cluster.tf
@@ -0,0 +1,35 @@
+resource "kind_cluster" "this" {
+  name           = local.name
+  wait_for_ready = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role = "control-plane"
+
+      kubeadm_config_patches = [
+        "kind: InitConfiguration\nnodeRegistration:\n  kubeletExtraArgs:\n    node-labels: \"ingress-ready=true\"\n"
+      ]
+
+      extra_port_mappings {
+        container_port = 80
+        host_port      = 80
+      }
+      extra_port_mappings {
+        container_port = 443
+        host_port      = 443
+      }
+    }
+
+    node {
+      role = "worker"
+    }
+
+    node {
+      role = "worker"
+    }
+  }
+
+}
