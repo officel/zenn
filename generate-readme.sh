93d70aaddbaf5f30de30dfc6415195814d08fcaa
NISHIMURA Yoshitaka <officel@users.noreply.github.com>
2025-10-20 15:19:04 +0900

    fix: memo link to md, publication marked
    - :memo: をマークダウンのリンクにして戻した
    - publication の出し分けをやってみたが、あんまりいい感じにならなかった
    - 別途 publication はリストを分割したほうがいいなと

--- generate-readme.sh
+++ generate-readme.sh
@@ -26,6 +26,20 @@ while IFS= read -r file; do
 
   [ -z "$title" ] && title="${filename%.*}"
 
+  # front matter publication_name 抽出
+  publication_name=$(awk '
+    BEGIN { in_front_matter=0; publication_name="" }
+    /^---$/ { in_front_matter = !in_front_matter; next }
+    in_front_matter && /^publication_name:/ {
+      sub(/^publication_name:[ \t]*/, ""); publication_name=$0
+      gsub(/^["'\'' ]+|["'\'' ]+$/,"",publication_name)
+      exit
+    }
+    END { print publication_name }
+  ' "$file")
+
+  [ -z "$publication_name" ] && publication_name="none"
+
   year=""
   if [[ "$filename" =~ ^([0-9]{4})-[0-9]{2}-[0-9]{2}_ ]]; then
     year="${BASH_REMATCH[1]}"
@@ -36,7 +50,8 @@ while IFS= read -r file; do
      --arg title    "$title" \
      --arg date     "$date_prefix" \
      --arg year     "$year" \
-    '. + [{"path":$path,"filename":$filename,"title":$title,"date":$date,"year":$year}]' articles.json \
+     --arg pub_name "$publication_name" \
+    '. + [{"path":$path,"filename":$filename,"title":$title,"date":$date,"year":$year,"publication_name":$pub_name}]' articles.json \
     > articles.tmp.json
   mv articles.tmp.json articles.json
 done < <(find articles -type f -name '*.md')
